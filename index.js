5d3d06e28a636016601b985a75e297be1dd3800e
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-10-26 15:59:54 -0400

    fixed looping through answers. displaying right or wrong answer

--- index.js
+++ index.js
@@ -1,100 +1,101 @@
 /* eslint-disable no-undef */
 'use strict';
+function main(){
+  const quizData = { 
+    questions: [
+      {
+        question: 'Which of the following is not part of the  harry potter book series?',
+        answers: [
+          'Order Of the Phoenix',
+          'Fantasic Beast',
+          'Prisoner of Azkaban',
+          'Chamber of Secrets',
+        ],
+        correctAnswer: 'Fantasic Beast',
+      },
+      {
+        question: 'What was the make and model of the flying car, that first appeared in Chamber of Secrets movie?',
+        answers: [
+          'Mini Cooper',
+          'Vauxhall Astra',
+          'Ford Anglia',
+          'Reliant Robin',
+        ],
+        correctAnswer: 'Ford Anglia',
+      },
+      {
+        question: 'In Goblet of Fire, who was the actor who played Cedric Diggory?',
+        answers: [
+          'Emma Watson',
+          'Rupert Grint',
+          'Alan Rickman',
+          'Robert Pattinson',
+        ],
+        correctAnswer: 'Rober Pattinson',
+      },
+      {
+        question: 'When did Harry Potter first encounter the Dementors?',
+        answers: [
+          'First encounter was on the train',
+          'First encounter was during a Quidditch match',
+          'First encounter was during the Order of the Phoenix',
+          'First encounter was when Harry blasts them off with a Patronus charm',
+        ],
+        correctAnswer: 'First encounter was on the train',
+      },
+      {
+        question: 'What is the identity of his Ron Weasley pet rat Scabbers?',
+        answers: [
+          'Percy',
+          'Sirius Black',
+          'Lily Potter',
+          'Peter Pettigrew',
+        ],
+        correctAnswer: 'Peter Pettigrew',
+      },
+    ],
+    currentQuestion: 0,
+    score: 0,
+  };
 
-const quizData = { 
-  questions: [
-    {
-      question: 'Which of the following is not part of the  harry potter book series?',
-      answers: [
-        'Order Of the Phoenix',
-        'Fantasic Beast',
-        'Prisoner of Azkaban',
-        'Chamber of Secrets',
-      ],
-      correctAnswer: 2,
-    },
-    {
-      question: 'What was the make and model of the flying car, that first appeared in Chamber of Secrets movie?',
-      answers: [
-        'Mini Cooper',
-        'Vauxhall Astra',
-        'Ford Anglia',
-        'Reliant Robin',
-      ],
-      correctAnswer: 3,
-    },
-    {
-      question: 'In Goblet of Fire, who was the actor who played Cedric Diggory?',
-      answers: [
-        'Emma Watson',
-        'Rupert Grint',
-        'Alan Rickman',
-        'Robert Pattinson',
-      ],
-      correctAnswer: 4,
-    },
-    {
-      question: 'When did Harry Potter first encounter the Dementors?',
-      answers: [
-        'First encounter was on the train',
-        'First encounter was during a Quidditch match',
-        'First encounter was during the Order of the Phoenix',
-        'First encounter was when Harry blasts them off with a Patronus charm',
-      ],
-      correctAnswer: 1,
-    },
-    {
-      question: 'What is the identity of his Ron Weasley pet rat Scabbers?',
-      answers: [
-        'Percy',
-        'Sirius Black',
-        'Lily Potter',
-        'Peter Pettigrew',
-      ],
-      correctAnswer: 4,
-    },
-  ],
-  currentQuestion: 0,
-  score: 0,
-};
-
-function startQuiz(){
-  $('.start-quiz').on('click',function(){
-    console.log('clicked');
-    renderAQuestion();
-  });
-}
-//Displays current question number your on
-function updateQuestion(){
-  $('.question').html(`<p id="js-answered"> 
-      Questions Number: ${quizData.currentQuestion + 1}/${quizData.questions.length}
+  function startQuiz(){
+    $('.start-quiz').on('click',function(){
+      console.log('clicked');
+      renderAQuestion();
+    });
+  }
+  //Displays current question number your on
+  function updateQuestion(){
+    $('.question').html(`<p id="js-answered"> 
+      Question: ${quizData.currentQuestion + 1}/${quizData.questions.length}
   </p>`);
-}
-//Displays score number
-function updateScore(){
-  $('.score').html(`<span id="js-score">
+  }
+  //Displays score number
+  function updateScore(){
+    $('.score').html(`<span id="js-score">
   Score: ${quizData.score}/${quizData.questions.length}
   </span>`);
   
-}
-//Display the answers for the current question
-function updateAnswers(){
-  let question = quizData.questions[quizData.currentQuestion];
-  //loop through answers and print
-  for(let i in quizData.questions){
-    console.log(question.answers[i]);
-    $('.js-answers').append(`<input type="radio" name="answers" id="option${i}" value="${question.answers[i]}" tabindex="${i}">
-    <label for="answers${i}"> ${question.answers[i]}</label><br>
-    <!-- will be used later to display information for wrong or right answer<span id="js-r${i}">test</span>-->`);
-  }// test span is commented out directly inside <!-- will be used later to display information for wrong or right answer<span id="js-r${i}">test</span>-->
-}
+  }
+  //Display the answers for the current question
+  function updateAnswers(){
+    let question = quizData.questions[quizData.currentQuestion];
+    //loop through answers and print
 
-function renderAQuestion(){
-  let question = quizData.questions[quizData.currentQuestion];
-  updateQuestion();
-  updateScore();
+    for(let i = 0; i < question.answers.length; i++){
+      $('.js-answers').append(`<input type="radio" name="answers" id="option${i}" class="answer-options" value="${question.answers[i]}" tabindex="${i}">
+    <label for="answers${i}"> ${question.answers[i]}</label><br/>
+    <span id="js-r${i}" class="answer-opitions"></span>`);
+    }
+  }
+  //render question
+  //html main form layout
+  function renderAQuestion(){
+    let question = quizData.questions[quizData.currentQuestion];
+    updateQuestion();
+    updateScore();
 
-  $('.main').html(`<div>
+    $('.main').html(`<div>
   <form id="js-questions" class="question-form">
     
       <div class="row question">
@@ -112,45 +113,81 @@ function renderAQuestion(){
   
     <div class="row">
       <div class="col-12">
-        <button type = "submit" id="answer" tabindex="5">Submit</button>
-        <button type = "button" id="next-question" tabindex="6"> Next >></button>
+        <button type="submit" id="answer" tabindex="5">Submit</button>
+        <button type="button" id="next-question" tabindex="6"> Next >></button>
       </div>
     </div>
   </form>
 </div>`);
-  updateAnswers();
-  //hides the 'next' button before submitting it
-  $('#next-question').hide();
+    updateAnswers();
+    //hides the 'next' button before submitting it
+    $('#next-question').hide();
+  }
+
+  function displayResults(){
+
+  }
+
+  function handleQuestions(){
+    $('form').on('click','#next-question',function(){
+      if(quizData.currentQuestion === quizData.questions.length){
+        displayResults();
+      } else {
+        renderAQuestion();
+      }
+    });
+  }
+  //checks if right or wrong answer and displays respective message
+  function handleSelectOption(){
+    $('.main').on('submit','.question-form',function(event){
+      event.preventDefault();
+
+      let currentQuest = quizData.questions[quizData.currentQuestion];
+      let selectedAnswer = $('input[type=radio]:checked').val();
+    
+      if(!selectedAnswer){
+        alert('choose an option');
+        return;
+      }
+      if(selectedAnswer === quizData.correctAnswer){
+        console.log('correct');
+      } else {
+        console.log('wrong answer');
+      }
+
+      let id_num = currentQuest.answers.findIndex(i => i === selectedAnswer);
+      let id = '#js-r' + ++id_num;
+      $('span').removeClass('right-answer wrong-answer');
+      if(selectedAnswer === currentQuest.correctAnswer){
+        quizData.score++;
+        $(`${id}`).append(`CORRECT <br/>`);
+        $(`${id}`).addClass('right-answer');
+      } else {
+        $(`${id}`).append(`The answer is "${currentQuest.correctAnswer}"<br/>`);
+        $(`${id}`).addClass('wrong-answer');
+      }
+    
+      quizData.currentQuestion++;
+      quizData.currentQuestion++;
+      $('#js-score').text(`Score: ${quizData.score}/${quizData.questions.length}`);
+      $('#answer').hide();
+      $('input[type=radio]').attr('disabled', true);
+      $('#next-question').show();
+    });
+  }
+
+  function restartQuiz(){
+
+  }
+
+  function handleQuizApp() {
+    startQuiz();
+    handleQuestions();
+    handleSelectOption();
+    restartQuiz();
+  }
+
+  $(handleQuizApp);
+
 }
-
-function handleQuestions(){
-
-}
-//checks for if right or wrong answer
-function handleSelectOption(){
-  $('form').on('sumbit','#js-questions',function(e){
-    e.preventDefault();
-
-    let currentQuestion = quizData.questions[quizData.currentQuestion];
-    let selectedAnswer = $('input[name^=answers]:checked').val();
-
-    if(selectedAnswer){
-      console.log(selectedAnswer);
-    }
-
-  });
-}
-
-function restartQuiz(){
-
-}
-
-function handleQuizApp() {
-  startQuiz();
-  handleQuestions();
-  handleSelectOption();
-  restartQuiz();
-}
-
-$(handleQuizApp);
-
+$(main);
