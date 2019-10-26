615cfaf4aa84932989a5339cbcc16c8583f4fa46
Christina Chapman <cmchapman75@gmail.com>
2019-10-26 13:16:00 -0500

    working in version 2

---
+++ script2.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const startButton = document.getElementById('start-btn');
+
+
+startButton.addEventListener('click', startGame);
+
+
+function startGame() {
+  console.log('Started');
+
+}
+
+function setNextQuestion(){
+
+}
+
+function selectAnswer() {
+
+}
